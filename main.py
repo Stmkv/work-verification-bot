7dabdfe2aa3a283c80365137896a6df7421a8845
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-10-30 22:59:43 +0300

    feat(bot):send logs to Telegram to admin user.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import textwrap
-import time
 import logging
+import time
+
 import requests
 import telegram
 from environs import Env
+
+logger = logging.getLogger("work_verification")
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def send_message(bot, tg_chat_id, result):
@@ -25,8 +38,14 @@ def main():
     tg_chat_id = env.str("TG_CHAT_ID")
     devman_token = env.str("DEVMAN_TOKEN")
     bot = telegram.Bot(token=tg_bot_token)
-    logging.warning("Бот Запущен!")
 
+    FORMAT = "%(asctime)s : %(name)s : %(levelname)s : %(message)s"
+    logging.basicConfig(
+        level=logging.INFO,
+        format=FORMAT,
+        handlers=[TelegramLogsHandler(bot, tg_chat_id)],
+    )
+    logger.info("Бот запущен")
     url = "https://dvmn.org/api/long_polling/"
     header = {"Authorization": devman_token}
     params = {}
@@ -43,8 +62,11 @@ def main():
             params.update({"timestamp": timestamp})
 
         except requests.exceptions.ReadTimeout:
+            logger.info("ReadTimeout")
+            time.sleep(1)
             continue
         except requests.exceptions.ConnectionError:
+            logger.info("ConnectionError")
             time.sleep(10)
             continue
 
